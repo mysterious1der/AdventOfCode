4c26e94424877f6b8d8ba2fb84550ee59062ae6c
mysterious1der <jirah@cox5.com>
2020-12-04 00:30:56 -0500

    Day 3 Complete

---
+++ 2020/Day3/project3part1.ps1
@@ -0,0 +1,30 @@
+﻿$input = Get-Content C:\Users\Jirah\Documents\GitHub\AdventOfCode\2020\Day3\input.txt 
+$countlines = $input | Measure-Object -Line | Select-Object -expandProperty Lines
+$countcols = $input[0] | Measure-Object -Character | Select-Object -expandProperty Characters
+
+#write-host $countlines
+#Write-Host $countcols
+
+#write-host $input[0]
+write-host $input[11].Substring(0,1)
+
+#for (($x = 0), ($y = 0); $y -lt $count)
+for (($x = 0), ($y = 0), ($trees = 0); $y -lt $countlines-1)
+{
+    $x += 3
+    $y ++
+
+
+    if ($x -ge $countcols) {
+        $x = $x - $countcols
+    }
+
+    #write-host "On line" $y ", X is" $x
+    Write-Host "Character at pos" $x "on line" $y "is" $input[$y].Substring($x,1)
+   
+    if ($input[$y].Substring($x,1) -eq "#")
+    {
+        $trees++
+    }
+}
+write-host "You hit" $trees "trees"
---
+++ 2020/Day3/project3part2.ps1
@@ -0,0 +1,94 @@
+﻿$input = Get-Content C:\Users\Jirah\Documents\GitHub\AdventOfCode\2020\Day3\input.txt 
+$countlines = $input | Measure-Object -Line | Select-Object -expandProperty Lines
+$countcols = $input[0] | Measure-Object -Character | Select-Object -expandProperty Characters
+
+#slope 1,1
+
+for (($x = 0), ($y = 0), ($trees = 0); $y -lt $countlines-1)
+{
+    $x += 1
+    $y ++
+
+    if ($x -ge $countcols) 
+    {
+        $x = $x - $countcols
+    }
+ 
+    if ($input[$y].Substring($x,1) -eq "#")
+    {
+        $trees11++
+    }
+}
+
+#slope 3,1
+
+for (($x = 0), ($y = 0), ($trees = 0); $y -lt $countlines-1)
+{
+    $x += 3
+    $y ++
+
+    if ($x -ge $countcols) 
+    {
+        $x = $x - $countcols
+    }
+ 
+    if ($input[$y].Substring($x,1) -eq "#")
+    {
+        $trees31++
+    }
+}
+
+#slope 5,1
+
+for (($x = 0), ($y = 0), ($trees = 0); $y -lt $countlines-1)
+{
+    $x += 5
+    $y ++
+
+    if ($x -ge $countcols) 
+    {
+        $x = $x - $countcols
+    }
+ 
+    if ($input[$y].Substring($x,1) -eq "#")
+    {
+        $trees51++
+    }
+}
+
+#slope 7,1
+
+for (($x = 0), ($y = 0), ($trees = 0); $y -lt $countlines-1)
+{
+    $x += 7
+    $y ++
+
+    if ($x -ge $countcols) 
+    {
+        $x = $x - $countcols
+    }
+ 
+    if ($input[$y].Substring($x,1) -eq "#")
+    {
+        $trees71++
+    }
+}
+
+#slope 1,2
+
+for (($x = 0), ($y = 0), ($trees = 0); $y -lt $countlines-1)
+{
+    $x ++
+    $y += 2
+
+    if ($x -ge $countcols) 
+    {
+        $x = $x - $countcols
+    }
+ 
+    if ($input[$y].Substring($x,1) -eq "#")
+    {
+        $trees12++
+    }
+}
+Write-Host "Multiplied trees =" ($trees11 * $trees31 * $trees51 * $trees71 * $trees12)
