3454e61c244b4a11baa6abf3a0ce5f6dadf4f57d
mysterious1der <jirah@cox5.com>
2020-12-08 17:18:38 -0500

    Day6

---
+++ 2020/Day6/project6part1.ps1
@@ -0,0 +1,11 @@
+﻿$input = [IO.File]::ReadAllText("C:\Users\Jirah\Documents\GitHub\AdventOfCode\2020\Day6\input.txt")
+$forms = $input -split '\r?\n\r?\n'
+$count = 0
+
+foreach ($form in $forms)
+{
+    $form = $form.ToCharArray() | Where { -not [string]::IsNullOrWhiteSpace($_) } | sort -CaseSensitive -Unique
+    $count += $form.Length
+}
+
+write-host $count "total answers"
---
+++ 2020/Day6/project6part2.ps1
@@ -0,0 +1,33 @@
+﻿$input = [IO.File]::ReadAllText("C:\Users\Jirah\Documents\GitHub\AdventOfCode\2020\Day6\input.txt")
+$forms = $input -split '\r?\n\r?\n'
+$count = 0
+$letters = 0
+
+foreach ($form in $forms)
+{
+    $temp = $form -split '\r?\n'
+
+    #Alphabet Loop
+    for ($tick = 0; $tick -lt 26; $tick++) 
+    { 
+        $letters = 0
+        $char = [char](97 +$tick) 
+        write-host "Working on letter" $char "on text " $temp        
+        for ($tock = 0; $tock -lt $temp.Length; $tock++)
+        {
+            if ($temp[$tock].Contains($char))
+            {
+                Write-Host "Found me a" $char
+                $letters++
+            }
+        }
+        $letters
+        if ($letters -eq $temp.length)
+        {
+            $count++
+            Write-Host "Looks like" $char "appears on every line here"
+        }
+    }
+}
+
+write-host "I have no idea if this is the answer but lets try" $count
